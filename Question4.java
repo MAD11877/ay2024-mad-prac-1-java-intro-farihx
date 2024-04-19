e491ce45f3559cff27d3da7450dfc923c6cc2793
farihx <167218485+farihx@users.noreply.github.com>
2024-04-19 16:14:05 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,23 +1,15 @@
 import java.util.Scanner;
 
-public class Question4
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
-    Scanner in = new Scanner(System.in);
-    
-  }
+public class Question4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        for (int i = size; i > 0; i--) {
+            // Loop to control columns
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
