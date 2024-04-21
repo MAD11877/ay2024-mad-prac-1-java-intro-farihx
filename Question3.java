d63d436cb381e089cf947854f35438db33260887
farihx <167218485+farihx@users.noreply.github.com>
2024-04-21 16:28:34 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
-public class Question3 {
-    public static void main(String[] args) {
+public class Question3
+{
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        int result = multiplyByItself(num);
-        System.out.println(result);
-    }
-    public static int multiplyByItself(int num) {
-        return num * num;
+
+        System.out.print("Enter an integer: ");
+        int number = in.nextInt();
+
+        int result = number * number;
+        System.out.println("Result: " + result);
     }
 }
