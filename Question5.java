816494b1756fcb8377886d5a9c43e118e812db41
farihx <167218485+farihx@users.noreply.github.com>
2024-04-21 16:36:33 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,29 +1,42 @@
 import java.util.Scanner;
 
-public class Question5 {
-    public static void main(String[] args) {
-       
+public class Question5
+{
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
-        int count = in.nextInt();
-        int[] numbers = new int[count];
-        for (int i = 0; i < count; i++) {
-            numbers[i] = in.nextInt();
+
+        System.out.print("Enter number of integers: ");
+        int cnt = in.nextInt();
+
+        int[] num = new int[count];
+
+        System.out.println("Enter integers:");
+        for (int i = 0; i < cnt; i++)
+        {
+            num[i] = in.nextInt();
         }
+        
         int mode = 0;
-        int maxCount = 0;
-        for (int i = 0; i < count; i++) {
-            int currentNumber = numbers[i];
-            int currentCount = 0;
-            for (int j = 0; j < count; j++) {
-                if (numbers[j] == currentNumber) {
-                    currentCount++;
+        int maxFreq = 0;
+        
+        for (int i = 0; i < cnt; i++)
+        {
+            int freq = 0;
+            for (int j = 0; j < cnt; j++)
+            {
+                if (num[i] == num[j])
+                {
+                    freq++;
                 }
             }
-            if (currentCount > maxCount) {
-                mode = currentNumber;
-                maxCount = currentCount;
+            if (freq > maxFreq)
+            {
+                mode = num[i];
+                maxFreq = freq;
             }
         }
-        System.out.println(mode);
+
+        System.out.println("Mode: " + mode);
     }
 }
