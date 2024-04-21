b4bdef29d3f61f3bd3af0a7613ef299fbf52da89
farihx <167218485+farihx@users.noreply.github.com>
2024-04-21 16:31:30 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
 
-public class Question4 {
-    public static void main(String[] args) {
+public class Question4
+{
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
-        int size = in.nextInt();
-        for (int i = size; i > 0; i--) {
-            // Loop to control columns
-            for (int j = 0; j < i; j++) {
+        
+        System.out.print("Enter an integer: ");
+        int base = in.nextInt();
+
+        for (int i = base; i > 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
                 System.out.print("*");
             }
             System.out.println();
