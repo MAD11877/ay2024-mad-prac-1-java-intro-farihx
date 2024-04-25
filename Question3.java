11ca54dc91f360ea768273ad68592aacc2da9751
farihx <167218485+farihx@users.noreply.github.com>
2024-04-25 21:45:16 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -5,11 +5,9 @@ public class Question3
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-
-        System.out.print("Enter an integer: ");
         int number = in.nextInt();
-
         int result = number * number;
         System.out.println("Result: " + result);
     }
 }
+//print
