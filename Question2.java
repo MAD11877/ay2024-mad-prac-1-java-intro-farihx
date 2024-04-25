1ca4cb41fddd54b9da6bbc95ff7d7608f4383db8
farihx <167218485+farihx@users.noreply.github.com>
2024-04-25 21:43:38 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Question2
 {
-    public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in);
-        double height = in.nextDouble();
-        in.nextLine();
-        double weight = in.nextDouble();
-        double bmi = weight / (height * height);
-    }
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
+    double height = in.nextDouble();
+    double weight = in.nextDouble();
+    double bmi = weight / (height * height);
+    String finalbmi = String.format("%.1f", bmi);
+    System.out.println(finalbmi);    
+    in.close();
+  }
 }
 //print
