37d701707f642fbde03c44f74b99f2a421176b52
farihx <167218485+farihx@users.noreply.github.com>
2024-04-25 21:47:20 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
 
 public class Question5 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
        
         Scanner in = new Scanner(System.in);
         int count = in.nextInt();
         int[] numbers = new int[count];
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++)
+        {
             numbers[i] = in.nextInt();
         }
         int mode = 0;
         int maxCount = 0;
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++)
+        {
             int currentNumber = numbers[i];
             int currentCount = 0;
-            for (int j = 0; j < count; j++) {
-                if (numbers[j] == currentNumber) {
+            for (int j = 0; j < count; j++)
+            {
+                if (numbers[j] == currentNumber)
+                {
                     currentCount++;
                 }
             }
-            if (currentCount > maxCount) {
+            if (currentCount > maxCount)
+            {
                 mode = currentNumber;
                 maxCount = currentCount;
             }
@@ -27,3 +33,4 @@ public class Question5 {
         System.out.println(mode);
     }
 }
+//print
