0d5a25cea610fdf0c090fdc1e8b8df94add8cc32
farihx <167218485+farihx@users.noreply.github.com>
2024-06-13 19:00:31 +0800

    Final? Question4.java

--- Question4.java
+++ Question4.java
@@ -2,22 +2,17 @@ import java.util.Scanner;
 
 public class Question4
 {
-    public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in);
-        
-        System.out.print("Enter an integer: ");
-        int base = in.nextInt();
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
+    int baseLength = in.nextInt();
 
-        for (int i = base; i > 0; i--)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        in.close();
+    for (int i = baseLength; i > 0; i--) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
     }
+    in.close();
+  }
 }
-//print
