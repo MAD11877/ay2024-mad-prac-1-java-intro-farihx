c713b47144ebc3a085b2bf6afce622a331fdc77d
farihx <167218485+farihx@users.noreply.github.com>
2024-06-13 19:06:27 +0800

    Final? Question5.java

--- Question5.java
+++ Question5.java
@@ -1,36 +1,34 @@
 import java.util.Scanner;
 
-public class Question5 {
-    public static void main(String[] args)
-    {
-       
-        Scanner in = new Scanner(System.in);
-        int count = in.nextInt();
-        int[] numbers = new int[count];
-        for (int i = 0; i < count; i++)
-        {
-            numbers[i] = in.nextInt();
-        }
-        int mode = 0;
-        int maxCount = 0;
-        for (int i = 0; i < count; i++)
-        {
-            int currentNumber = numbers[i];
-            int currentCount = 0;
-            for (int j = 0; j < count; j++)
-            {
-                if (numbers[j] == currentNumber)
-                {
-                    currentCount++;
-                }
-            }
-            if (currentCount > maxCount)
-            {
-                mode = currentNumber;
-                maxCount = currentCount;
-            }
-        }
-        System.out.println(mode);
+public class Question5
+{
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int[] numbers = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      numbers[i] = in.nextInt();
     }
+
+    int mode = numbers[0];
+    int maxCount = 0;
+
+    for (int i = 0; i < n; i++) {
+      int count = 0;
+      for (int j = 0; j < n; j++) {
+        if (numbers[j] == numbers[i]) {
+          count++;
+        }
+      }
+      if (count > maxCount) {
+        maxCount = count;
+        mode = numbers[i];
+      }
+    }
+
+    System.out.println(mode);
+    in.close();
+  }
 }
-//print
