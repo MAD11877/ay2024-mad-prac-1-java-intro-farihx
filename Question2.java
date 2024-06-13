0a9e727a2b05d80dcdb3af70414068079683d05d
farihx <167218485+farihx@users.noreply.github.com>
2024-06-13 19:09:22 +0800

    Final? Question2.java

--- Question2.java
+++ Question2.java
@@ -5,12 +5,13 @@ public class Question2
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
+    
     double height = in.nextDouble();
     double weight = in.nextDouble();
     double bmi = weight / (height * height);
+    
     String finalbmi = String.format("%.1f", bmi);
     System.out.println(finalbmi);    
     in.close();
   }
 }
-//print
