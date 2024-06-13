0774e26cbd1ef21b72666d40a4ea6f2a9a355aca
farihx <167218485+farihx@users.noreply.github.com>
2024-06-13 18:49:20 +0800

    Final? Question3.java

--- Question3.java
+++ Question3.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Question3
 {
-    public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
-        int result = number * number;
-        System.out.println("Result: " + result);
-    }
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
+    int result = num * num;
+    System.out.println(result);    
+    in.close();
+  }
 }
-//print
